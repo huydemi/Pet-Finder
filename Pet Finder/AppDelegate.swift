e870b7ae1d7340efe34318c064fe5fdcbbc61584
Dang Quoc Huy <huysamdua@yahoo.com>
2018-07-22 22:55:45 +0700

    Automating dark theme with Solar

--- Pet Finder/AppDelegate.swift
+++ Pet Finder/AppDelegate.swift
@@ -26,11 +26,35 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
+  private let solar = Solar(latitude: 40.758899, longitude: -73.9873197)!
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     
-    Theme.current.apply()
+    initializeTheme()
     
     return true
   }
+  
+  func initializeTheme() {
+    if solar.isDaytime {
+      Theme.current.apply()
+      scheduleThemeTimer()
+    } else {
+      Theme.dark.apply()
+    }
+  }
+  
+  func scheduleThemeTimer() {
+    let timer = Timer(fire: solar.sunset!, interval: 0, repeats: false) { [weak self] _ in
+      Theme.dark.apply()
+      
+      self?.window?.subviews.forEach({ (view: UIView) in
+        view.removeFromSuperview()
+        self?.window?.addSubview(view)
+      })
+    }
+    
+    RunLoop.main.add(timer, forMode: RunLoopMode.commonModes)
+  }
+  
 }
